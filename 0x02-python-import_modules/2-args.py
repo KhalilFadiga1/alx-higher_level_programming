b4910d7f824957b637ccbb4b33b9d3b9ac184150
KhalilFadiga1 <khalilfadiga667@gmail.com>
2024-01-19 10:55:11 -0800

    Update 2-args file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     from sys import argv 
     numOfArg = len(argv) - 1
     if numOfArg == 0:
-        print("0 arguments.")
+        print("{:d} arguments.".format(numOfArg))
     elif numOfArg == 1:
-        print("1 argument:")
+        print("{:d} argument:".format(numOfArg))
     else:
         print("{:d} arguments:".format(numOfArg))
     for x in range(numOfArg):
-        print("{:d} : {:s}".format(x + 1, sys.argv[x + 1]))
+        print("{:d}: {:s}".format(x + 1, sys.argv[x + 1]))
 
