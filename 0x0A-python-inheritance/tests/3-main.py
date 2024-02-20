5c6006dc46f298aaf3fab9859ed7f36c30dbe1ca
KhalilFadiga1 <khalilfadig667@gmail.com>
2024-02-20 03:06:19 -0800

    Updates

--- 0x0A-python-inheritance/tests/3-main.py
+++ 0x0A-python-inheritance/tests/3-main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 is_kind_of_class = __import__('3-is_kind_of_class').is_kind_of_class
-
 a = 1
 if is_kind_of_class(a, int):
     print("{} comes from {}".format(a, int.__name__))
