9e73fa5022f484949104bf5f9f33c302579893a1
KhalilFadiga1 <khalilfadig667@gmail.com>
2024-02-20 05:33:29 -0800

    Update

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,13 +4,16 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Initialise a new Rectangle.
+    """Represent a rectangle using BaseGeometry."""
 
-    Args:
-        width (int): The width of the new Rectangle.
-        height (int): The height of the rectangle.
-    """
-    self.integer_validator("width", width)
-    self.__width = width
-    self.integer_validator("height", height)
-    self.__height = height
+    def __init__(self, width, height):
+        """Initialise a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -6,8 +6,8 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """Represent a rectangle using BaseGeometry."""
 
-    def __int__(self, width, height):
-         """Intialize a new Rectangle.
+    def __init__(self, width, height):
+        """Intialize a new Rectangle.
 
         Args:
             width (int): The width of the new Rectangle.
@@ -23,5 +23,5 @@ class Rectangle(BaseGeometry):
     def __str__(self):
         """Return the print() and str() representation of a Rectangle."""
         string = "[" + str(self.__class__.__name__) + "]"
-        string += str(self.__width) + "/" str(self.__height)
+        string += " " + str(self.__width) + "/" + str(self.__height)
         return string
