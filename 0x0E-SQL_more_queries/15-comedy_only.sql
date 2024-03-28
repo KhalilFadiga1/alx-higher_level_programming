4b6f4da0b4c0c570c93987f714e459f40efe7cff
KhalilFadiga1 <khalilfadiga667@gmail.com>
2024-03-28 02:30:14 -0700

    Make Corrections

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
 -- uses a databse to lists all rows in a table corresponding to all rows in another
 SELECT title
-FROM tv_genres
+FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
