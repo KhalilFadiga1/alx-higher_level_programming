4736486fdb82ff4e817e9269b7d00bf240ecabfd
KhalilFadiga1 <khalilfadiga667@gmail.com>
2024-04-24 01:01:44 -0700

    Using repeat method to print a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const argTwo = process.argv[2];
+const changeStr = parseInt(argTwo);
+
+if (isNaN(argTwo)) {
+  console.log('Missing size');
+} else {
+  let i, alphabet = 'X';
+  for (i = 0; i < changeStr; i++) {
+    console.log(alphabet.repeat(changeStr));
+  }
+}
