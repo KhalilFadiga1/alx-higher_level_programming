f7c5e237382b41106c17f8b2f9b3a174e6a9ef53
KhalilFadiga1 <khalilfadiga667@gmail.com>
2024-04-28 01:58:50 -0700

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that list all states from the hbtn_0e_0_usa database."""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    #Get MySQL credentials from command-line arguments
+    #Connecting MySQL server
+    db = MySqLdb.connect(user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3])
+    curs = db.cursor()
+
+    #Executing the SQL query to retrieve all states sorted by id
+    curs.execute("SELECT * FROM states ORDER BY `id`")
+    [print(state) for state in curs.fetchall() if state[1][0] == "N"]
