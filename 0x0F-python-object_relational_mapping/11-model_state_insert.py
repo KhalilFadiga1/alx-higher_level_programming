53ca6ff8099a36433f6a529d9ed6d99f092b8762
KhalilFadiga1 <khalilfadiga667@gmail.com>
2024-04-30 13:06:44 -0700

    11-model_state_insert

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module that adds a new state to a MySQL database using SQLAlchemy"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+if __name__ == "__main__":
+    #Create the SqlAlchemy engine using the available MySQL credentials
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                            pool_pre_ping = True)
+
+    #Create a session factory
+    session = sessionmaker(bind = engine)
+
+    #Create a session object
+    session = session()
+
+    #Create a new State object for Louisiana
+    louisiana = State(name = "Lousiana")
+    
+    #Add the new state to session
+    session.add(louisiana)
+
+    #Commit the session to persist the changes
+    session.commit()
+
+    #Print the ID of the new state
+    print(lousiana.id)
