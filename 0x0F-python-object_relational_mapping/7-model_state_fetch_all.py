44cf5c10f39b0801dcdc0dce931a97d6c1b7c4ca
KhalilFadiga1 <khalilfadiga667@gmail.com>
2024-04-30 10:47:56 -0700

    Update to 7-model_state_fetch_all

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,7 +7,7 @@ from model_state import State
 
 if __name__ == "__main__":
     #Create the SqlAlchemy engine using the available MySQL credentials
-    engine = create_engine("mysql+mysqldb://{}:{}#localhost/{}"
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                             .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                             pool_pre_ping = True)
 
