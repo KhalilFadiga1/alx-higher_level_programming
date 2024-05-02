a60b8c911b8e6899af7a461034754cfa0ae6ee54
KhalilFadiga1 <khalilfadiga667@gmail.com>
2024-05-02 13:16:57 -0700

    update

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,18 +6,21 @@ import MySQLdb
 
 if __name__ == "__main___":
     #Get MySQL credentials and search name from cml args
-    db = MySQLdb.connect(user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3])
+    db = MySQLdb.connect(host="localhost", user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3], pport=3306)
 
     #Connectth the MySQL server
     c = db.cursor()
 
     #Execute the SQL query to retrieve cities in the specified state
-    query = ("SELECT * \
-                FROM citites as `c` \
-                INNER JOIN  `states` as `s` \
-                    ON `c`, `state_id` = `s`, `id` \
-                ORDER BY `c`, `id`")
+    query = ("SELECT cities.name \
+                FROM citites \
+                INNER JOIN  states ON states.id = cities.state_id \
+                WHERE states.name=%s", (sys.argv[4],))
 
     #Fetch all rows and filter cities by the specified states
     #Print the cities
     print(", ".join([ct[2] for ct in ct.fetchall() if ct[4] == sys.argv[4]]))
+
+    #Close the server and the database
+    c.close()
+    db.close()
